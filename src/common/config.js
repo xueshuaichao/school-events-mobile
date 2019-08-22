f5bd3b68554cfc46458918d39f3de00ee3385a42
wangyuzju <wangyu0248@gmail.com>
2019-08-22 10:57:17 +0800

    fix: bug

--- src/common/config.js
+++ src/common/config.js
@@ -1,3 +1,11 @@
+// 进行 环境判断 时，dev 模式 process.env.NODE_ENV 的值为 development，
+// build 模式 process.env.NODE_ENV 的值为 production。
+const ENV = process.env.NODE_ENV;
+const HOST = {
+    development: 'http://aitiaozhan.wdyclass.com:8000',
+    production: 'http://aitiaozhan.test.wdyclass.com:8000',
+};
+
 /* eslint-disable no-restricted-globals */
 let host;
 
@@ -6,7 +14,7 @@ host = `${location.protocol}//${location.host}`; /* 根域名不同 */
 // #endif
 
 // #ifndef H5
-host = 'http://aitiaozhan.wdyclass.com:8000';
+host = HOST[ENV];
 // #endif
 
 export default {
