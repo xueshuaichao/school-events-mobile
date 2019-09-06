df9713903b109f131401c42b9c8595d2cbd52b1d
wangyuzju <wangyu0248@gmail.com>
2019-09-06 12:12:08 +0800

    fix: fix bug

--- src/common/config.js
+++ src/common/config.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-undef */
 // 进行 环境判断 时，dev 模式 process.env.NODE_ENV 的值为 development，
 // build 模式 process.env.NODE_ENV 的值为 production。
-const API_ENV = process.env.API_ENV || 'dev';
-const HOST = {
-    dev: 'http://aitiaozhan.wdyclass.com:8000',
-    test: 'http://aitiaozhan.test.wdyclass.com:8000',
-    online: 'http://aitiaozhan.wdyclass.com',
-};
+// const API_ENV = process.env.API_ENV || 'dev';
+
+console.log('api env:');
+console.log(SERVER_HOST);
 
 /* eslint-disable no-restricted-globals */
 let host;
@@ -15,7 +14,7 @@ host = `${location.protocol}//${location.host}`; /* 根域名不同 */
 // #endif
 
 // #ifndef H5
-host = HOST[API_ENV];
+host = SERVER_HOST;
 // #endif
 
 export default {
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+
+const API_ENV = process.env.API_ENV || 'test';
+console.log('API ENV ---:');
+console.log(API_ENV);
+
+const HOST = {
+    dev: 'http://aitiaozhan.wdyclass.com:8000',
+    test: 'http://aitiaozhan.test.wdyclass.com:8000',
+    online: 'https://aitiaozhan.wdyedu.com',
+};
+
+module.exports = {
+    configureWebpack: () => ({
+        plugins: [
+            new webpack.DefinePlugin({
+                SERVER_HOST: `"${HOST[API_ENV]}"`,
+            }),
+        ],
+    }),
+};
