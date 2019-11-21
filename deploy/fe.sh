c6a9bf125327db8c1f89a414d84b2454b95336be
wangyuzju <wangyu0248@gmail.com>
2019-11-21 18:34:06 +0800

    fix: update deploy script

--- deploy/fe.sh
+++ deploy/fe.sh
@@ -12,7 +12,7 @@ if [ -d "./node_modules/wdc-vue-components" ];then
     rm -rf ./node_modules/wdc-vue-components/
 fi
 npm i
-npm run build
+# npm run build
 if [[ $1 = "online" ]];then
     echo "npm run host:pro";
     npm run build:api_online
