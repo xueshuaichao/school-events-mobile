f91a574ff500d7d9239e42276f6da602481537c1
wangyuzju <wangyu0248@gmail.com>
2020-01-11 16:37:38 +0800

    fix: fix bug

--- src/pages/work/festival/festival.vue
+++ src/pages/work/festival/festival.vue
@@ -636,7 +636,6 @@ export default {
         width: 100%;
         height: 100%;
         background: rgba(0, 0, 0, 0.7);
-        background: red;
         text-align: center;
 
         .share-pic {
