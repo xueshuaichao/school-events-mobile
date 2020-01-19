22e4fdd89bb9a628fbc5c3221ccce80000758a0d
wangyuzju <wangyu0248@gmail.com>
2020-01-19 11:24:29 +0800

    feat:  add hot fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <script>
 export default {
     onLaunch() {
-        console.log('App Launch');
+        // #ifdef MP-WEIXIN
+        // eslint-disable-next-line no-undef
+        const updateManager = wx.getUpdateManager();
+
+        updateManager.onCheckForUpdate((res) => {
+            // 请求完新版本信息的回调
+            console.log(res.hasUpdate);
+        });
+
+        updateManager.onUpdateReady(() => {
+            // eslint-disable-next-line no-undef
+            wx.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success(res) {
+                    if (res.confirm) {
+                        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                        updateManager.applyUpdate();
+                    }
+                },
+            });
+        });
+
+        updateManager.onUpdateFailed(() => {
+            // 新版本下载失败
+        });
+        // #endif
     },
     onShow() {
         console.log('App Show');
