1b870d2d4dfd121fb1de9984f3ed9e009e997799
吴红月 <768028030@qq,com>
2020-05-06 11:01:40 +0800

    feat: 红包雨

---
+++ src/components/packet-rain/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <view class="packet-rain">
+        <view
+            v-for="(item, index) in packetList"
+            :key="index"
+        >
+            <image
+                src=""
+                mode=""
+            />
+        </view>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            packetList: [],
+        };
+    },
+};
+</script>
