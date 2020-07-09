28f5a1b1f3f601d75f710034b5717d421a8f8533
吴红月 <768028030@qq,com>
2020-07-09 17:00:57 +0800

    feat: 测试部署流程

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,15 +5,15 @@ IMGTAG=$2
 
 
 if [[ $ENV = 'online' ]]; then
-      docker login -u=wdcloudtoc -p=wdcloudInf@2019 registry.cn-beijing.aliyuncs.com
-      docker build -t registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:${IMGTAG} -f deploy/Dockerfile .
-      docker tag registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:${IMGTAG} registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:latest
-      docker push registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:${IMGTAG}
-      docker push registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:latest
-      docker logout registry.cn-beijing.aliyuncs.com
+    docker login -u=wdcloudtoc -p=wdcloudInf@2019 registry.cn-beijing.aliyuncs.com
+    docker build -t registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:${IMGTAG} -f deploy/Dockerfile .
+    docker tag registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:${IMGTAG} registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:latest
+    docker push registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:${IMGTAG}
+    docker push registry.cn-beijing.aliyuncs.com/wdcloudinf/aitiaozhan-c-mobile:latest
+    docker logout registry.cn-beijing.aliyuncs.com
 else
-      docker build -t dockerhub.wdcloud.cc/school-events-mobile-fe-${ENV}:${IMGTAG} -f deploy/Dockerfile .
-      docker push  dockerhub.wdcloud.cc/school-events-mobile-fe-${ENV}:${IMGTAG}
+    docker build -t dockerhub.wdcloud.cc/school-events-mobile-fe-${ENV}:latest -f deploy/Dockerfile .
+    docker push  dockerhub.wdcloud.cc/school-events-mobile-fe-${ENV}:latest
 fi
 
 
