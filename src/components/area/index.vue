df0cf770b1ccb3b4ca66aa1a422ea924e5457859
吴红月 <768028030@qq,com>
2020-07-10 18:51:15 +0800

    feat: 打卡活动

---
+++ src/components/area/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <my-picker
+        class="form-item-cont"
+        :picker-list="provinceList"
+        :default-value="defaultValue"
+        @confirm="confirm($event)"
+    >
+        <view>
+            <view class="uni-input placeholder fake-input">
+                请选择
+            </view>
+        </view>
+    </my-picker>
+</template>
+
+<script>
+import myPicker from '../picker/picker.vue';
+import Area from './data';
+
+export default {
+    components: {
+        myPicker,
+    },
+    props: {
+        default: {
+            type: Array,
+            default() {
+                return [];
+            },
+        },
+        addData: {
+            type: Object,
+            default() {
+                return {};
+            },
+        },
+        resource: {
+            type: String,
+            default: '',
+        },
+    },
+    data() {
+        return {
+            newArea: {},
+            defaultValue: [],
+        };
+    },
+    watch: {
+        default: {
+            handler(val) {
+                this.defaultValue = val;
+            },
+            immediate: true,
+        },
+    },
+    created() {
+        if (!this.resource) {
+            this.provinceList = Area;
+        } else {
+            const newArea = JSON.parse(
+                JSON.stringify(Area.filter(v => v.value === this.resource)),
+            );
+            if (Object.keys(this.addData).length) {
+                newArea[0].children.push(this.addData);
+            }
+            this.provinceList = newArea;
+        }
+    },
+    methods: {
+        confirm(picked) {
+            this.$emit('change', picked);
+        },
+    },
+};
+</script>
+
+<style lang="less">
+.form-item-cont {
+    width: 100%;
+    height: 100upx;
+}
+</style>
