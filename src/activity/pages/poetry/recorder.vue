47bc1baf1fd9da3c3cf952fb726a5befb06853fb
lily <catyumouse@sina.com>
2020-08-03 15:19:56 +0800

    feat: add folder

---
+++ src/activity/pages/poetry/recorder.vue
@@ -0,0 +1,19 @@
+<template>
+    <view>
+        asasa
+    </view>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            recorder: null,
+        };
+    },
+    created() {
+        this.recorder = uni.getRecorderManager();
+    },
+    methods: {},
+};
+</script>
+<style lang="less" scoped></style>
