078e4707368576f0befcd8fbe06bc5cd16381052
lily <catyumouse@sina.com>
2020-08-06 13:59:35 +0800

    feat: ui

--- src/pages/uc/setting/setting.vue
+++ src/pages/uc/setting/setting.vue
@@ -8,6 +8,9 @@
             @login="onLogin"
         />
         <template v-else>
+            <view class="p-title">
+                个人信息
+            </view>
             <view
                 class="item"
                 @click="settingImg"
@@ -28,8 +31,9 @@
                     姓名
                 </text>
                 <view class="arrow">
+                    <!--游客可以修改姓名信息-->
                     <template
-                        v-if="userInfo.identity === 1"
+                        v-if="userInfo.identity !== 1"
                         @click="settingName"
                     >
                         <text v-if="userInfo.name">
@@ -41,10 +45,7 @@
                         >
                             未设置
                         </text>
-                        <image
-                            class="arrow-r"
-                            src="/static/images/uc/r-arrow.png"
-                        />
+                        <text class="arrow-r" />
                     </template>
                     <template v-else>
                         {{ userInfo.name }}
@@ -78,33 +79,6 @@
                     <text>老师</text>
                 </view>
             </template>
-            <template v-if="userInfo.identity === 4">
-                <!-- 学生 -->
-                <view class="item">
-                    <text class="text">
-                        学校
-                    </text>
-                    <text>{{ userInfo.student_info.school_name }}</text>
-                </view>
-                <view class="item">
-                    <text class="text">
-                        年级
-                    </text>
-                    <text>{{ userInfo.student_info.grade_name }}</text>
-                </view>
-                <view class="item">
-                    <text class="text">
-                        班级
-                    </text>
-                    <text>{{ userInfo.student_info.class_name }}</text>
-                </view>
-                <view class="item">
-                    <text class="text">
-                        用户名
-                    </text>
-                    <text>{{ userInfo.user_account }}</text>
-                </view>
-            </template>
             <navigator
                 class="item"
                 url="/pages/uc/setting/resetPassword"
@@ -112,12 +86,7 @@
                 <text class="text">
                     重置密码
                 </text>
-                <view class="arrow">
-                    <image
-                        class="arrow-r"
-                        src="/static/images/uc/r-arrow.png"
-                    />
-                </view>
+                <text class="arrow-r" />
             </navigator>
             <view class="item">
                 <text>绑定手机号</text>
@@ -125,6 +94,73 @@
                     {{ userInfo.mobile }}
                 </text>
             </view>
+            <template v-if="userInfo.identity === 4">
+                <!-- 学生 -->
+                <view class="p-title item">
+                    <view>
+                        学校信息
+                    </view>
+                    <view
+                        class="blue"
+                        @click="show = true"
+                    >
+                        <text class="br">
+                            ?
+                        </text>
+                        <text class="txt">
+                            调班/升学
+                        </text>
+                    </view>
+                </view>
+                <view class="item">
+                    <text class="text">
+                        学校
+                    </text>
+                    <template v-if="userInfo.student_info.school_name">
+                        <text>{{ userInfo.student_info.school_name }}</text>
+                    </template>
+                    <template v-else>
+                        <view>
+                            <text class="none">
+                                暂无学校
+                            </text>
+                            <text class="arrow-r" />
+                        </view>
+                    </template>
+                </view>
+                <view class="item">
+                    <text class="text">
+                        班级
+                    </text>
+                    <template v-if="userInfo.student_info.grade_name">
+                        <text>
+                            {{ userInfo.student_info.grade_name || ""
+                            }}{{ userInfo.student_info.class_name || "" }}
+                        </text>
+                    </template>
+                    <template v-else>
+                        <view>
+                            <text class="none">
+                                暂无班级
+                            </text>
+                            <text class="arrow-r" />
+                        </view>
+                    </template>
+                </view>
+                <view class="item">
+                    <text class="text">
+                        学生码
+                    </text>
+                    <view>
+                        <text>
+                            {{ userInfo.ID_number }}
+                        </text>
+                        <text class="copy">
+                            复制
+                        </text>
+                    </view>
+                </view>
+            </template>
             <view
                 class="btn"
                 @click="doLogout"
@@ -132,6 +168,39 @@
                 退出登录
             </view>
         </template>
+        <view
+            v-if="show"
+            class="tip-modal"
+        >
+            <view class="modal-ctx">
+                <view class="top">
+                    提示
+                </view>
+                <view class="body">
+                    <view class="tip">
+                        转校和升学的方法和注意事项：
+                    </view>
+                    <view class="txt more-txt">
+                        点击所在学校或所在班级，依次选择需要转入的学校和班级即可（切记选择准确，否则会影响后续活动成绩）
+                    </view>
+                    <view class="txt">
+                        注意事项：
+                    </view>
+                    <view class="txt">
+                        1.转校或转班后，您之前的成绩不会被带到新的学校或班级。
+                    </view>
+                    <view class="txt">
+                        2.在转校和升学前，请联系原学校管理员将您的账号做离校/升学操作。
+                    </view>
+                    <view
+                        class="btns"
+                        @click="show = false"
+                    >
+                        知道了
+                    </view>
+                </view>
+            </view>
+        </view>
     </view>
 </template>
 <script>
@@ -156,6 +225,7 @@ export default {
                 mobile: '',
                 identity: '',
             },
+            show: false,
         };
     },
     methods: {
@@ -294,6 +364,7 @@ export default {
 <style lang="less">
 .page-setting {
     padding: 4rpx 32rpx 0;
+    color: #000;
     .item {
         height: 110rpx;
         display: flex;
@@ -301,19 +372,26 @@ export default {
         align-items: center;
         border-bottom: 1rpx solid #ddd;
         font-size: 30rpx;
+        padding: 0 2upx;
     }
     .img {
         width: 92rpx;
         height: 92rpx;
         border-radius: 50%;
     }
-    .arrow {
-        color: #333;
-    }
     .arrow-r {
-        width: 12upx;
-        height: 22upx;
-        margin-left: 10upx;
+        width: 16upx;
+        height: 16upx;
+        border-top: 2upx solid #333;
+        border-right: 2upx solid #333;
+        transform: rotate(45deg);
+        display: inline-block;
+        position: relative;
+        bottom: 2upx;
+        margin-left: 14upx;
+    }
+    .none {
+        color: #999;
     }
     .btn {
         background-color: #1166ff;
@@ -323,6 +401,88 @@ export default {
         color: #fff;
         margin-top: 80upx;
         text-align: center;
+        font-size: 32upx;
+    }
+    .copy {
+        color: #1166ff;
+        margin-left: 16upx;
+    }
+    .p-title {
+        font-size: 32upx;
+        font-weight: 500;
+        margin: 24upx 0;
+        display: flex;
+        justify-content: space-between;
+        .blue {
+            color: #1166ff;
+            .br {
+                width: 30upx;
+                height: 30upx;
+                border-radius: 50%;
+                border: 2upx solid #1166ff;
+                text-align: center;
+                line-height: 30upx;
+                margin-right: 6upx;
+                display: inline-block;
+                font-size: 24upx;
+            }
+            .txt {
+                position: relative;
+                bottom: -2upx;
+            }
+        }
+    }
+    .tip-modal {
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        position: fixed;
+        left: 0;
+        top: 0;
+        z-index: 1000;
+        .modal-ctx {
+            width: 640upx;
+            margin: 120upx auto 0;
+            background: #fff;
+
+            .top {
+                height: 84upx;
+                text-align: center;
+                background: #1166ff;
+                font-size: 32upx;
+                color: #fff;
+                line-height: 84upx;
+                border-radius: 2upx;
+            }
+            .body {
+                padding: 0 40upx 40upx;
+            }
+            .tip {
+                font-size: 30upx;
+                color: #000;
+                line-height: 120upx;
+            }
+            .txt {
+                font-size: 26upx;
+                line-height: 42upx;
+                color: #333;
+                margin-bottom: 20upx;
+                &.more-txt {
+                    margin-bottom: 40upx;
+                }
+            }
+            .btns {
+                width: 360upx;
+                margin: 40upx auto 0;
+                height: 98upx;
+                line-height: 98upx;
+                font-size: 32upx;
+                background: #1166ff;
+                color: #fff;
+                text-align: center;
+                border-radius: 2upx;
+            }
+        }
     }
 }
 </style>
